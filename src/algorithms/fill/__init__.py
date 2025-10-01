14ab55091757f4dabf22ead66e5759a01ca864c5
Polina-Fox <115147066+Polina-Fox@users.noreply.github.com>
2025-10-01 16:51:46 +0300

    надеюсь последняя правка

--- src/algorithms/fill/__init__.py
+++ src/algorithms/fill/__init__.py
@@ -1,5 +1,5 @@
-from .flood_fill import FloodFill
-from .pattern_fill import PatternFill
-from .boundary_tracer import BoundaryTracer
+from algorithms.fill.flood_fill import FloodFill
+from algorithms.fill.pattern_fill import PatternFill
+from algorithms.fill.boundary_tracer import BoundaryTracer
 
 __all__ = ['FloodFill', 'PatternFill', 'BoundaryTracer']
